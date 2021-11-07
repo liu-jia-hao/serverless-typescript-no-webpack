2d430796f5b2fa4be30a8069804217f156072fac
DESKTOP-0P848CA\edinl_6z7c6t <edinliu@gmail.com>
2021-11-08 06:23:39 +0800

    Do lazy initialization of global variables

--- helpers/gcp.ts
+++ helpers/gcp.ts
@@ -18,6 +18,12 @@ export const firebaseAdmin = (): admin.app.App => {
   return admin;
 };
 
+// Declared at cold-start, but only initialized if/when the function executes
+let lazyFirestore: admin.firestore.Firestore;
 export const firestore = (): admin.firestore.Firestore => {
-  return firebaseAdmin().firestore();
+  if (!lazyFirestore) {
+    lazyFirestore = firebaseAdmin().firestore();
+    return lazyFirestore;
+  }
+  return lazyFirestore;
 };
